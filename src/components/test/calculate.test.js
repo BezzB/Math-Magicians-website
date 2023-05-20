8ce70fd613be9b0584d0c1177e4bb0435e1b3e00
BezzB <118984781+BezzB@users.noreply.github.com>
2023-05-20 09:55:33 +0300

    Update add modulo tests

--- src/components/test/calculate.test.js
+++ src/components/test/calculate.test.js
@@ -58,4 +58,36 @@ describe('test calculation', () => {
     };
     expect(calculate(sampleInputData, '=')).toEqual(expectedOutput);
   });
+
+  // Test multiplication
+  it('Should return 25', () => {
+    const sampleInputData = {
+      total: '5',
+      next: '5',
+      operation: 'x',
+    };
+
+    const expectedOutput = {
+      total: '25',
+      next: null,
+      operation: null,
+    };
+    expect(calculate(sampleInputData, '=')).toEqual(expectedOutput);
+  });
+
+  // Test modulo
+  it('Should return 0', () => {
+    const sampleInputData = {
+      total: '10',
+      next: '5',
+      operation: '%',
+    };
+
+    const expectedOutput = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    expect(calculate(sampleInputData, '=')).toEqual(expectedOutput);
+  });
 });
